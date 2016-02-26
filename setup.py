bd5a222ffeca5416ba4d70618160d21c30bac8e8
Koh Wei Jie <weijie.koh@yale-nus.edu.sg>
2016-02-26 14:10:30 +0800

    Enabled Mac installation

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import platform
 
 from Client import connect
 
-# if nix then run installer
-if platform.system() == "Linux":
+# if nix or Mac then run installer
+if platform.system() == "Linux" or platform.system() == "Darwin":
     # give installer a null value
     installer = False
 
@@ -34,27 +34,29 @@ if platform.system() == "Linux":
     # if user specified install then lets proceed to the installation
     if installer is True:
 
-	# if we trigger on sources.list then we know its ubuntu
-	if os.path.isfile("/etc/apt/sources.list"):
+        # install openssh-server with apt for Debian systems
+        if platform.system() == "Linux":
+            # if we trigger on sources.list then we know its Debian
+            if os.path.isfile("/etc/apt/sources.list"):
 
-	    # force install of debian packages
-	    subprocess.Popen("apt-get --force-yes -y install openssh-server", shell=True).wait()
+                # force install of debian packages
+                subprocess.Popen("apt-get --force-yes -y install openssh-server", shell=True).wait()
+            # if sources.list is not available then we're running something offset
+            else: 
+                print("[!] You're not running a Debian variant. Installer not finished for this type of Linux distro.")
+                print("[!] Install open-ssh server manually for all of autossh dependencies.")
+                sys.exit()
 	 
-	    if 'ServerAliveInterval' not in open('/etc/ssh/ssh_config').read():
-    		writetoSSH = open('/etc/ssh/ssh_config','a')
-    		writetoSSH.write("    ServerAliveInterval 30\n    ServerAliveCountMax 4")
-    		writetoSSH.close()
+        if 'ServerAliveInterval' not in open('/etc/ssh/ssh_config').read():
+            writetoSSH = open('/etc/ssh/ssh_config','a')
+            writetoSSH.write("    ServerAliveInterval 30\n    ServerAliveCountMax 4")
+            writetoSSH.close()
 
-	    if 'ClientAliveInterval' not in open('/etc/ssh/sshd_config').read():
-	        writetoSSH = open('/etc/ssh/sshd_config','a')
-    		writetoSSH.write("ClientAliveInterval 30\nClientAliveCountMax 4")
-    		writetoSSH.close()
+        if 'ClientAliveInterval' not in open('/etc/ssh/sshd_config').read():
+            writetoSSH = open('/etc/ssh/sshd_config','a')
+            writetoSSH.write("ClientAliveInterval 30\nClientAliveCountMax 4")
+            writetoSSH.close()
 
-	# if sources.list is not available then we're running something offset
-	else: 
-	    print("[!] You're not running a Debian variant. Installer not finished for this type of Linux distro.")
-	    print("[!] Install open-ssh server manually for all of autossh dependencies.")
-	    sys.exit()
 	
 	# if installation is done on client, the autossh automatically kicks in the daemon
 	try:
@@ -94,9 +96,8 @@ if platform.system() == "Linux":
 	    print("[!] Installation has failed. Please ensure that connect.py and .pub file is installed")
 
 # if the platform is running on a MAC, a version will be ready soon
-if platform.system() == 'Darwin':
-    print("[!] A version for Mac will be ready soon")
+# if platform.system() == 'Darwin':
+    # print("[!] A version for Mac will be ready soon")
 
-if platform.system() != "Linux":
-   if platform.system() != "Darwin":
+if platform.system() != "Linux" and platform.system() != "Darwin":
 	print("[!] Sorry this installer is not designed for any other system other than Linux or Mac. Please install the python dependencies manually.")
