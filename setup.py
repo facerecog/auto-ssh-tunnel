c3ded05f1976f74c677d8e81b6c48568c24dc95f
Koh Wei Jie <weijie.koh@yale-nus.edu.sg>
2016-02-26 14:38:26 +0800

    Enabled mac installation

--- setup.py
+++ setup.py
@@ -74,9 +74,9 @@ if platform.system() == "Linux" or platform.system() == "Darwin":
                 subprocess.Popen("yes | cp Client/connect.py /etc/init.d/", shell=True).wait()
                 subprocess.Popen("chmod +x /etc/init.d/connect.py", shell=True).wait()
             elif platform.system() == "Darwin":
-                subprocess.Popen("mkdir /System/Library/StartupItems/auto-ssh-tunnel", shell=True).wait()
-                subprocess.Popen("yes | cp mac/StartupParameters.plist /System/Library/StartupItems/auto-ssh-tunnel/", shell=True).wait()
-                subprocess.Popen("yes | cp Client/connect.py /System/Library/StartupItems/auto-ssh-tunnel/", shell=True).wait()
+                subprocess.Popen("mkdir /System/Library/StartupItems/auto-ssh-tunnel", shell=True)
+                subprocess.Popen("yes | cp mac/StartupParameters.plist /System/Library/StartupItems/auto-ssh-tunnel/", shell=True)
+                subprocess.Popen("yes | cp Client/connect.py /System/Library/StartupItems/auto-ssh-tunnel/", shell=True)
                 subprocess.Popen("chmod +x /System/Library/StartupItems/auto-ssh-tunnel/connect.py", shell=True).wait()
 
             subprocess.call("printf 'priv_key\n\n' | ssh-keygen -t rsa -b 2048 -v -P ''", shell=True)
